4ac7a7f49e36878775e2ee60907ec0a4b7fca547
dosumis <dosumis@gmail.com>
2020-06-03 18:37:49 +0100

    First commit. Works but needs some clean up.

---
+++ src/ccf_tools.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import rdflib
+
+def parse_CCF_tsv(path):
+    ccf_tsv = pd.read_csv(path, sep='\t', skipinitialspace=True)
+    lookup = ccf_tsv[['ID', 'Label (indented)']]
+    part_cols = ccf_tsv.drop(columns=['ID', 'Label (indented)'])
+    out = pd.DataFrame(columns=['o', 's', 'olabel', 'slabel'])
+    col_pairs = []
+    for current, nekst in zip(part_cols.columns, part_cols.columns[1:]):
+        col_pair = part_cols[[current, nekst]].drop_duplicates().dropna().rename(columns={current: 'olabel', nekst: 'slabel'})
+        col_pairs.append(col_pair)
+    out = pd.concat(col_pairs, ignore_index=True)
+    fu = out.merge(lookup, left_on=['olabel'], right_on='Label (indented)').drop(columns=['Label (indented)']).rename(columns={'ID': 'o'})
+    bar = fu.merge(lookup, left_on=['slabel'], right_on='Label (indented)').drop(columns=['Label (indented)']).rename(columns={'ID': 's'})
+    return bar
+
+
+def get_ccf_owl():
+    g = rdflib.Graph()
+    g.parse('http://purl.org/ccf/latest/ccf.owl')
+    return g
+
+def ccf_owl_2_part_rels():
+    g = get_ccf_owl()
+    ccf_po_query = """
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+PREFIX ccf: <https://purl.org/ccf/latest/ccf.owl#> 
+SELECT DISTINCT ?o ?s ?olabel ?slabel
+   WHERE { ?s ccf:ccf_part_of ?o . 
+           ?o rdfs:label ?olabel .
+           ?s rdfs:label ?slabel .
+       FILTER(STRSTARTS(STR(?o), "http://purl.obolibrary.org/obo/UBERON_")) 
+       FILTER(STRSTARTS(STR(?s), "http://purl.obolibrary.org/obo/UBERON_")) 
+    }"""
+    return g.query(ccf_po_query)
---
+++ src/uberongraph_tools.py
@@ -0,0 +1,37 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+class UberonGraph():
+    def __init__(self):
+        self.sparql = SPARQLWrapper('https://stars-app.renci.org/uberongraph/sparql')
+        self.sparql.setReturnFormat(JSON)
+        self.ask_uberon_po = """
+PREFIX part_of: <http://purl.obolibrary.org/obo/BFO_0000050> 
+prefix UBERON: <http://purl.obolibrary.org/obo/UBERON_>
+ASK
+FROM <http://reasoner.renci.org/ontology>
+FROM <http://reasoner.renci.org/redundant>
+{ %s part_of: %s }"""
+        self.ask_uberon_overlaps = """
+PREFIX overlaps: <http://purl.obolibrary.org/obo/RO_0002131> 
+prefix UBERON: <http://purl.obolibrary.org/obo/UBERON_>
+ASK
+FROM <http://reasoner.renci.org/ontology>
+FROM <http://reasoner.renci.org/redundant>
+ { %s overlaps: %s }"""
+
+        self.ask_uberon_subclassof = """
+prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+prefix UBERON: <http://purl.obolibrary.org/obo/UBERON_>
+ASK FROM <http://reasoner.renci.org/ontology/closure>
+    { %s rdfs:subClassOf %s }"""
+
+    def ask_uberon(self, r, q, urls=True):
+        start = ''
+        end = ''
+        if urls:
+            start = '<'
+            end = '>'
+        q = q % (start + r['s'] + end, start + r['o'] + end)
+        self.sparql.setQuery(q)
+        results = self.sparql.query().convert()
+        return results['boolean']
