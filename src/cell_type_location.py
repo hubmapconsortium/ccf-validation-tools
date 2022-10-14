2bf64dee8ea0246137db16c2128240d1908c88fb
Anita Caron <anitacaron@users.noreply.github.com>
2022-10-14 21:14:53 +0100

    generate robot templates and remove blood vasculature table from merged report

--- src/cell_type_location.py
+++ src/cell_type_location.py
@@ -1,8 +1,12 @@
 from argparse import ArgumentParser
+from itertools import product
 import re
 import pandas as pd
 import os
 import json
+
+from uberongraph_tools import UberonGraph
+from ccf_tools import split_terms, transform_to_str
 
 def get_ct_loc_marker(file):
   def is_valid_id(content):
@@ -60,9 +64,53 @@ def merge_csv(path):
   df = pd.concat([pd.read_csv(f'{dir_path}/{f}', sep='\t') for f in csv_files ], ignore_index=True).drop_duplicates().sort_values("Terminal CT/ID")
 
   df.to_csv(path, sep='\t', index=False)
-  
 
-def main(args):
+def create_template(input, relationship):
+  output = [{'ID': 'ID', 
+             'Label': 'LABEL',
+             'Equivalent axioms': 'EC %',
+             'Equivalent relationship axioms': f'EC {relationship} some %',
+            }]
+
+  for _, row in input.iterrows():
+    d = {}
+    d['Label'] = f'{row["Terminal CT/label"]} of {row["Terminal AS/label"]}'
+    d['Equivalent axioms'] = row["Terminal CT/ID"]
+    d['Equivalent relationship axioms'] = row["Terminal AS/ID"]
+    output.append(d)
+  return pd.DataFrame.from_records(output)
+  
+def template(args):
+  ug = UberonGraph()
+
+  report = pd.read_csv(args.input, sep='\t')
+
+  clean_report = report.groupby(['Terminal AS/ID','Terminal CT/ID']).filter(lambda x: len(x)>1)
+  clean_report.to_csv('clean_report.tsv', sep='\t')
+                    # leukocyte    myeloid cell
+  parent_classes = ["CL:0000738", "CL:0000763"]
+  ct_list = list(clean_report["Terminal CT/ID"].drop_duplicates())
+  combine = list(product(ct_list, parent_classes))
+
+  terms_pairs = transform_to_str(combine)
+
+  valid_subclass, not_valid_subclass = ug.verify_relationship(terms_pairs=terms_pairs, relationship=ug.select_subclass)
+
+  terms_ct_i, _ = split_terms(transform_to_str(valid_subclass))
+  ccf_input_temp = clean_report.loc[clean_report["Terminal CT/ID"].isin(terms_ct_i)]
+  
+  ccf_template = create_template(ccf_input_temp, "located in")
+  ccf_template.to_csv(args.output, sep='\t')
+
+  terms_ct_ni, _ = split_terms(not_valid_subclass)
+  terms_ct = set(terms_ct_ni) - set(terms_ct_i)
+  cl_input_temp = clean_report.loc[clean_report["Terminal CT/ID"].isin(terms_ct)]
+
+  cl_template = create_template(cl_input_temp, "part of")
+  cl_template.to_csv('../templates/cl_compound.tsv', sep='\t')
+
+
+def generate(args):
   report = get_ct_loc_marker(args.input)
 
   if report is not None:
@@ -73,10 +121,12 @@ if __name__ == '__main__':
   parser = ArgumentParser()
   parser.add_argument('input', help='input json file')
   parser.add_argument('output', help='output csv file')
-  parser.add_argument('mode', help='two modes to deal with report: generate or merge')
+  parser.add_argument('mode', help='three modes to deal with report: generate, merge or template')
 
   args = parser.parse_args()
   if args.mode == 'generate':
-    main(args)
+    generate(args)
   elif args.mode == 'merge':
     merge_csv(args.output)
+  elif args.mode == 'template':
+    template(args)
