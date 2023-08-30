fd62ad6583c973afcc12400ca357f4acbb3e8edf
Anita Caron <anitacaron@users.noreply.github.com>
2023-08-30 16:43:51 +0100

    add fail and pass color

--- src/dashboard_generation.py
+++ src/dashboard_generation.py
@@ -17,11 +17,35 @@ def add_link(report):
 
     return report
 
+def check_number_n_get_color(number):
+    PASS = "<font color='green'>"
+    FAIL = "<font color='red'>"
+    CLOSE_TAG = "</font>"
+    if float(number) < 50.0:
+        return f"{PASS}{number}{CLOSE_TAG}"
+    else:
+        return f"{FAIL}{number}{CLOSE_TAG}"
+    
+
+def add_color(report, report_type):
+    if report_type == "terms":
+        for row in report.itertuples():
+            report.at[row.Index, "AS_invalid_term_percent"] = check_number_n_get_color(row.AS_invalid_term_percent)
+            report.at[row.Index, "CT_invalid_term_percent"] = check_number_n_get_color(row.CT_invalid_term_percent)
+    elif report_type == "relations":
+        for row in report.itertuples():
+            report.at[row.Index, "percent_invalid_AS-AS_relationship"] = check_number_n_get_color(row._3)
+            report.at[row.Index, "percent_invalid_CT-CT_relationship"] = check_number_n_get_color(row._5)
+            report.at[row.Index, "percent_invalid_CT-AS_relationship"] = check_number_n_get_color(row._7)
+            
+    return report
+
 def get_reports(date):
     BASE_PATH = "../reports/report_"
     
     ter_report = pd.read_csv(f"{BASE_PATH}terms_{date}.tsv", sep='\t')
     ter_report.sort_values(by=["Table"], inplace=True)
+    ter_report = add_color(ter_report.reset_index(drop=True), "terms")
     ter_report.rename(columns={
         "AS_valid_term_number": "# VALID AS TERMS",
         "AS_invalid_term_number": "# INVALID AS TERMS",
@@ -30,11 +54,13 @@ def get_reports(date):
         "CT_invalid_term_number": "# INVALID CT TERMS",
         "CT_invalid_term_percent": "% INVALID CT TERMS"
     }, inplace=True)
-    ter_report = add_link(ter_report.reset_index(drop=True))
+    ter_report = add_link(ter_report)
     ter_report_md = tsv2md(ter_report)
     
     rel_report = pd.read_csv(f"{BASE_PATH}relationship_{date}.tsv", sep='\t')
     rel_report.sort_values(by=["Table"], inplace=True)
+    rel_report = clean_up(rel_report.reset_index(drop=True))
+    rel_report = add_color(rel_report, "relations")
     rel_report.rename(columns={
         "number_of_AS-AS_relationships": "# AS-AS RELATIONS",
         "percent_invalid_AS-AS_relationship": "% INVALID AS-AS RELATIONS",
@@ -43,16 +69,14 @@ def get_reports(date):
         "number_of_CT-AS_relationships": "# CT-AS RELATIONS",
         "percent_invalid_CT-AS_relationship": "% INVALID CT-AS RELATIONS"
     }, inplace=True) 
-    rel_report = add_link(clean_up(rel_report))
+    rel_report = add_link(rel_report)
     rel_report_md = tsv2md(rel_report)
     
     return ter_report_md, rel_report_md
 
 def generate_dashboard(output):
-    # DATE_FILE = date.today().strftime("%Y%m%d")
-    DATE_FILE = "20230810"
-    # DATE = date.today().strftime('%Y-%m-%d')
-    DATE = "2023-08-10"
+    DATE_FILE = date.today().strftime("%Y%m%d")
+    DATE = date.today().strftime('%Y-%m-%d')
 
     template = MdUtils(file_name=output, title=f'Validation Dashboard ({DATE})')
     
