49ad2cb848e4f8d138741b58456216cca5835679
Anita Caron <anitac@ebi.ac.uk>
2024-07-17 10:32:20 +0100

    Get only latest GLB file version for each Reference Organ (#333)
    * add docstring to hra_wrapper script and fix formatting
    
    * query 3d ref organs by graph names
    
    before it was getting all versions available for each uberon term; now
    it gets the latest version available for each organ
    
    * update hra_uberon_3d_images.owl with only latest glb files
    
    * sort values in the final pandas result instead of in the sparql querying for each graph
    
    * serialize owl to ofn to have the output sorted by term
    
    * update owl and csv files
    
    ---------
    
    Co-authored-by: Anita Caron <anitacaron@users.noreply.github.com>

--- src/hra_wrapper.py
+++ src/hra_wrapper.py
@@ -1,8 +1,85 @@
+"""
+Description: This script is used to query the Human Reference Atlas (HRA)
+SPARQL endpoint to get the 3D images of the reference organs.
+"""
 import pandas as pd
+from rdflib.graph import ConjunctiveGraph
 from SPARQLWrapper import JSON, RDFXML, SPARQLWrapper
+
+REF_ORGAN_BASE_URI = "https://purl.humanatlas.io/ref-organ/"
+GRAPH_NAME_LIST = [
+    "brain-female",
+    "brain-male",
+    "blood-vasculature-female",
+    "blood-vasculature-male",
+    "eye-female-left",
+    "eye-female-right",
+    "eye-male-left",
+    "eye-male-right",
+    "fallopian-tube-female-left",
+    "fallopian-tube-female-right",
+    "heart-female",
+    "heart-male",
+    "kidney-female-left",
+    "kidney-female-right",
+    "kidney-male-left",
+    "kidney-male-right",
+    "knee-female-left",
+    "knee-female-right",
+    "knee-male-left",
+    "knee-male-right",
+    "large-intestine-female",
+    "large-intestine-male",
+    "larynx-female",
+    "larynx-male",
+    "liver-female",
+    "liver-male",
+    "lung-female",
+    "lung-male",
+    "lymph-node-female",
+    "lymph-node-male",
+    "main-bronchus-female",
+    "main-bronchus-male",
+    "mammary-gland-female-left",
+    "mammary-gland-female-right",
+    "ovary-female-left",
+    "ovary-female-right",
+    "palatine-tonsil-female-left",
+    "palatine-tonsil-female-right",
+    "palatine-tonsil-male-left",
+    "palatine-tonsil-male-right",
+    "pancreas-female",
+    "pancreas-male",
+    "pelvis-female",
+    "pelvis-male",
+    "placenta-full-term-female",
+    "prostate-male",
+    "skin-female",
+    "skin-male",
+    "small-intestine-female",
+    "small-intestine-male",
+    "spinal-cord-female",
+    "spinal-cord-male",
+    "spleen-female",
+    "spleen-male",
+    "thymus-female",
+    "thymus-male",
+    "trachea-female",
+    "trachea-male",
+    "ureter-female-left",
+    "ureter-female-right",
+    "ureter-male-left",
+    "ureter-male-right",
+    "urinary-bladder-female",
+    "urinary-bladder-male",
+    "uterus-female"
+]
 
 
 class HRAWrapper():
+    """
+    Class to query the Human Reference Atlas (HRA) SPARQL endpoint.
+    """
     def __init__(self):
         self.sparql = SPARQLWrapper("https://lod.humanatlas.io/sparql")
         self.construct_images_uberon = """
@@ -15,22 +92,21 @@ class HRAWrapper():
             PREFIX ccf: <http://purl.org/ccf/>
             PREFIX anatomical_entity: <http://purl.obolibrary.org/obo/UBERON_0001062>
 
-            CONSTRUCT {
-            ?uberon foaf:depiction ?url_uri .
-            }
-            WHERE {
-            ?uberon rdfs:subClassOf anatomical_entity: .
-            ?x a owl:NamedIndividual .
-            ?x rdf:type ccf:SpatialEntity .
-            ?x rdf:type ?uberon .
-            ?x ccf:representation_of ?uberon .
-            ?x ccf:has_object_reference [
-                ccf:file_url ?url
-            ] .
-            FILTER (CONTAINS(STR(?url), "https://cdn.humanatlas.io/digital-objects/ref-organ/"))
-            FILTER (!CONTAINS(STR(?url), "https://cdn.humanatlas.io/digital-objects/ref-organ/united"))
-            BIND(STRDT(STR(?url), xsd:anyURI) as ?url_uri) .
-            }
+            CONSTRUCT {{
+                ?uberon foaf:depiction ?url_uri .
+            }}
+            WHERE {{
+                GRAPH {graph_name} {{
+                    ?uberon rdfs:subClassOf* anatomical_entity: .
+                    ?x rdf:type ccf:SpatialEntity .
+                    ?x rdf:type ?uberon .
+                    ?x ccf:representation_of ?uberon .
+                    ?x ccf:has_object_reference [
+                        ccf:file_url ?url
+                    ] .
+                    BIND(STRDT(STR(?url), xsd:anyURI) as ?url_uri) .
+                }}
+            }}
         """
         self.reference_organ_spatial_entity = """
             PREFIX owl: <http://www.w3.org/2002/07/owl#>
@@ -42,32 +118,36 @@ class HRAWrapper():
             PREFIX ccf: <http://purl.org/ccf/>
             PREFIX anatomical_entity: <http://purl.obolibrary.org/obo/UBERON_0001062>
 
-            SELECT ?representation_of ?glb_file
-            WHERE {
-                ?uberon rdfs:subClassOf anatomical_entity: .
-                ?x a owl:NamedIndividual .
-                ?x rdf:type ccf:SpatialEntity .
-                ?x rdf:type ?uberon .
-                ?x ccf:representation_of ?uberon .
-                ?x ccf:has_object_reference [
-                    ccf:file_url ?url
-                ] .
-                FILTER (CONTAINS(STR(?url), "https://cdn.humanatlas.io/digital-objects/ref-organ/"))
-                FILTER (!CONTAINS(STR(?url), "https://cdn.humanatlas.io/digital-objects/ref-organ/united"))
-                BIND(?uberon as ?representation_of) .
-                BIND(?url as ?glb_file) .
-            }
-            ORDER BY ?representation_of
+            SELECT DISTINCT ?representation_of ?glb_file
+            WHERE {{
+                GRAPH {graph_name} {{
+                    ?uberon rdfs:subClassOf* anatomical_entity: .
+                    ?x rdf:type ccf:SpatialEntity .
+                    ?x rdf:type ?uberon .
+                    ?x ccf:representation_of ?uberon .
+                    ?x ccf:has_object_reference [
+                        ccf:file_url ?url
+                    ] .
+                    BIND(?uberon as ?representation_of) .
+                    BIND(?url as ?glb_file) .
+                }}
+            }}
         """
-    
-    def query_hra(self, query, format):
+
+    def query_hra(self, query, format_result):
+        """
+        Query the HRA SPARQL endpoint and return the results.
+        """
         self.sparql.setQuery(query)
-        self.sparql.setReturnFormat(format=format)
+        self.sparql.setReturnFormat(format=format_result)
         result = self.sparql.query().convert()
-        
+
         return result
-    
+
     def extract_result(self, results):
+        """
+        Extract the results from the query in a simplified format.
+        """
         results_simplified = []
         for res in results:
             r = {}
@@ -79,12 +159,27 @@ class HRAWrapper():
 
 if __name__ == '__main__':
     hra = HRAWrapper()
-    images_link = hra.query_hra(hra.construct_images_uberon, RDFXML)
-    images_link_table = hra.extract_result(
-        hra.query_hra(
-            hra.reference_organ_spatial_entity, JSON
-        )["results"]["bindings"]
-    )
+    images_link = ConjunctiveGraph()
+    images_link_table = []
+    for graph_name in GRAPH_NAME_LIST:
+        graph_iri = f"<{REF_ORGAN_BASE_URI}{graph_name}>"
 
-    images_link.serialize('../owl/hra_uberon_3d_images.owl', format='xml')
-    pd.DataFrame.from_records(images_link_table).to_csv("../reports/hra_uberon_3d_ref_objects.csv", index=False)
+        images_link += hra.query_hra(
+            hra.construct_images_uberon.format(graph_name=graph_iri),
+            RDFXML
+        )
+        ilt = hra.extract_result(
+                hra.query_hra(
+                    hra.reference_organ_spatial_entity.format(
+                        graph_name=graph_iri
+                    ),
+                    JSON
+                )["results"]["bindings"]
+            )
+        if ilt:
+            images_link_table.extend(ilt)
+
+    images_link.serialize("../owl/hra_uberon_3d_images.owl", format="xml")
+    pd.DataFrame.from_records(images_link_table)\
+        .sort_values(by="representation_of")\
+        .to_csv("../reports/hra_uberon_3d_ref_objects.csv", index=False)
